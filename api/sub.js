10b07c2751ba8e05bc5b3e027f0bccd21dc42913
frontflg <50544382+frontflg@users.noreply.github.com>
2022-08-21 10:36:31 +0900

    Update sub.js

--- api/sub.js
+++ api/sub.js
@@ -11,11 +11,6 @@ exports.update = function(req, res){
   const name = req.body[0].name;
   const val  = req.body[0].value;
   console.log('UPDATE ' + id + ' ' + name + ' ' + val);
-  // プライマリーキー情報を取得
-  const sql = 'SELECT COLUMN_NAME FROM information_schema.COLUMNS' +
-              ' WHERE TABLE_NAME = "' + id + '"' +
-              ' AND COLUMN_KEY = "PRI" ORDER BY ORDINAL_POSITION';
-  res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
   let strKeys = '';
   let strNums = '';
   let sqlWhere = '';
@@ -23,6 +18,11 @@ exports.update = function(req, res){
   let sqlInsVal = '';
   let sqlUpdate = '';
   let isNum = 0;
+  // プライマリーキー情報を取得
+  const sql = 'SELECT COLUMN_NAME FROM information_schema.COLUMNS' +
+              ' WHERE TABLE_NAME = "' + id + '"' +
+              ' AND COLUMN_KEY = "PRI" ORDER BY ORDINAL_POSITION';
+  res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
   connection.query(sql, function (error, results) {
     if (error) {
       throw error; // エラー処理
@@ -101,11 +101,12 @@ exports.update = function(req, res){
                   res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
                   connection.query(sql4, function (error, results) {
                     if (error) {
+                      console.log(sql4);
                       throw error; // エラー処理
                     } else {
                       res.status(200).send();
                     }
-                  }
+                  })
                 }
                 // 既存なら、UPDATE を実行
                 if (cnt[0].CNT === 1) {
@@ -114,11 +115,12 @@ exports.update = function(req, res){
                   res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
                   connection.query(sql5, function (error, results) {
                     if (error) {
+                      console.log(sql5);
                       throw error; // エラー処理
                     } else {
                       res.status(200).send();
                     }
-                  }
+                  })
                 }
                 // 複数なら、更新なし
                 if (cnt[0].CNT > 1) {
@@ -128,6 +130,102 @@ exports.update = function(req, res){
             });
           } else {
             console.log('更新対象絞り込み不能！');
+          }
+        }
+      });
+    }
+  });
+};
+
+exports.delete = function(req, res){
+  const id = req.query.id;
+  const name = req.body[0].name;
+  const val  = req.body[0].value;
+  console.log('DELETE ' + id + ' ' + name + ' ' + val);
+  let strKeys = '';
+  let strNums = '';
+  let sqlWhere = '';
+  let isNum = 0;
+  // プライマリーキー情報を取得
+  const sql = 'SELECT COLUMN_NAME FROM information_schema.COLUMNS' +
+              ' WHERE TABLE_NAME = "' + id + '"' +
+              ' AND COLUMN_KEY = "PRI" ORDER BY ORDINAL_POSITION';
+  res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
+  connection.query(sql, function (error, results) {
+    if (error) {
+      throw error; // エラー処理
+    } else {
+      strKeys = Object.values(JSON.parse(JSON.stringify(results)));
+      // 数字項目情報を取得
+      const sql2 = 'SELECT COLUMN_NAME FROM information_schema.COLUMNS' +
+                  ' WHERE TABLE_NAME = "' + id + '"' +
+                  ' AND DATA_TYPE LIKE "%int%" ORDER BY ORDINAL_POSITION';
+      res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
+      connection.query(sql2, function (error, results) {
+        if (error) {
+          throw error; // エラー処理
+        } else {
+          strNums = JSON.stringify(results);
+          for (let i = 0; i < req.body.length; i++) {
+            // SQL WHERE句を編集
+            const itemName = req.body[i].name;
+            const itemValue = req.body[i].value;
+            isNum = 0;
+            for( var k = 0; k < strNums.length; k++ ){
+              if (strNums[k].COLUMN_NAME === itemName) {
+                isNum = 1;
+              }
+            }
+            for( var j = 0; j < strKeys.length; j++ ){
+              if (strKeys[j].COLUMN_NAME === itemName) {
+                if (sqlWhere === '') {
+                  sqlWhere = ' WHERE ' + itemName + ' = ';
+                } else {
+                  sqlWhere = sqlWhere + ' AND ' + itemName + ' = ';
+                }
+                if (isNum === 0) {
+                  sqlWhere = sqlWhere + '"' + itemValue + '"';
+                } else {
+                  sqlWhere = sqlWhere + itemValue;
+                }
+              }
+            }
+          };
+          if (sqlWhere) {
+            // 存在チェック
+            const sql3 = 'SELECT COUNT(*) AS CNT FROM ' + id + sqlWhere;
+            res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
+            console.log(sql3);
+            connection.query(sql3, function (error, results) {
+              if (error) {
+                throw error; // エラー処理
+              } else {
+                const cnt = results;
+                if (cnt[0].CNT === 0) {
+                  console.log('NOT FOUNDです 削除できません！');
+                }
+                // 既存なら、DELETE を実行
+                if (cnt[0].CNT === 1) {
+                  console.log('該当１件あり DELETEします！');
+                  const sql6 = 'DELETE FROM ' + id + sqlWhere;
+                  res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
+                  connection.query(sql6, function (error, results) {
+                    if (error) {
+                      console.log(sql6);
+                      throw error; // エラー処理
+                    } else {
+                      res.status(200).send();
+                    }
+                  })
+                }
+                // 複数なら、削除なし
+                if (cnt[0].CNT > 1) {
+                  console.log(cnt[0].CNT + '件あり 削除しません！');
+                }
+              }
+            });
+          } else {
+            console.log('削除対象絞り込み不能！');
           }
         }
       });
