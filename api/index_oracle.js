1b9af6659a998c576ea2f73eecbe5c78b467e9c5
frontflg <50544382+frontflg@users.noreply.github.com>
2022-10-23 16:17:25 +0900

    Update index_oracle.js

--- api/index_oracle.js
+++ api/index_oracle.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) { // app.get...(expressの構文)、req=request
 });
 
 // 汎用テーブル更新処理
-app.get('/update', function(req, res) {
+app.post('/update', function(req, res) {
   const id = req.query.id;
   const name = req.body[0].name;
   const val  = req.body[0].value;
@@ -154,7 +154,7 @@ app.get('/update', function(req, res) {
 };
 
 // 汎用テーブル更新処理
-app.get('/delete', function(req, res) {
+app.post('/delete', function(req, res) {
   const id = req.query.id;
   const name = req.body[0].name;
   const val  = req.body[0].value;
@@ -249,6 +249,52 @@ app.get('/delete', function(req, res) {
     }
   });
 };
+
+app.post('/bookupdate', function (req, res) { // app.post...(expressの構文)、req=request。 res=response
+  const isbn13 = req.body.ISBN13;
+  if (isbn13.trim().length > 13) {
+    console.log('UPDATE ID FAILER:' + isbn13);
+    throw error; // エラー処理
+  }
+  console.log('UPDATE ' + isbn13);
+  const isbn10    = req.body.ISBN10;
+  const bookName  = req.body.BookName;
+  const author    = req.body.Author;
+  const publisher = req.body.Publisher;
+  const genre     = req.body.Genre;
+  let issueDate = '0000/00/00';
+  if (req.body.IssueDate !== '') {
+    issueDate = req.body.IssueDate;
+  }
+  let getDate = '0000/00/00';
+  if (req.body.GetDate !== '') {
+    getDate = req.body.GetDate;
+  }
+  let readDate = '0000/00/00';
+  if (req.body.ReadDate !== '') {
+    readDate = req.body.ReadDate;
+  }
+  const ownership = req.body.Ownership;
+  const purchase  = req.body.Purchase;
+  const library   = req.body.Library;
+  const overview  = req.body.Overview;
+  const impress   = req.body.Impressions;
+  const state     = req.body.State;
+  const coverImg  = req.body.CoverImg;
+  const sql = 'UPDATE booklog SET ISBN10 = ?, BookName = ?, Author = ?, Publisher = ?, Genre = ?,' +
+              ' IssueDate = ?,GetDate = ?, ReadDate = ?, Ownership = ?, Purchase = ?, Library = ?,' +
+              ' Overview  = ?, Impressions = ?, State = ?, CoverImg = ? where ISBN13 = ?';
+  res.set({ 'Access-Control-Allow-Origin': '*' }); // この記載により、※1：CORSを許可する
+  connection.query(sql,[isbn10, bookName, author, publisher, genre, issueDate, getDate, readDate,
+                ownership, purchase, library, overview, impress, state, coverImg, isbn13],
+                function (error, results) {
+    if (error) {
+      throw error; // エラー処理
+    } else {
+      res.status(200).send();
+    }
+  });
+});
 
 // serverMiddleware導入で変更
 // app.listen(5000, function () { // port 5000をlistenする
